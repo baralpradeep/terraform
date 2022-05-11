d6689036e7392faf3bed97b9d650e1a61f527134
Pradeep Baral <52475976+baralpradeep@users.noreply.github.com>
2022-05-11 11:55:04 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "azurerm" {
   subscription_id = var.subscription_id
   client_id       = var.client_id
   client_secret   = var.client_secret
-  tenant_id       = var.tenanat_id
+  tenant_id       = var.tenant_id
 }
 
 #create resource group
