70e4d53baf4cdbdf578f31666349568c6335e3c6
Pradeep Baral <52475976+baralpradeep@users.noreply.github.com>
2022-05-11 21:17:41 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,7 +12,22 @@ resource "azurerm_resource_group" "rg" {
     name     = var.RGName
     location = var.location
 }
-#end
+
+#Create virtual network
+resource "azurerm_virtual_network" "vnet" {
+    name                = "vnet-dev-westus2-001"
+    address_space       = ["10.0.0.0/16"]
+    location            = var.location 
+    resource_group_name = azurerm_resource_group.rg.name
+}
+
+# Create subnet
+resource "azurerm_subnet" "subnet" {
+  name                 = "snet-dev-westus2-001 "
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefix       = "10.0.0.0/24"
+}
 
 
 
